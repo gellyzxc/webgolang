c417e5c72aeea4042d22de2c8b600e716f2ee6ad
Общий профиль ptpit <ptpit@pl1.edu>
2022-05-07 15:47:22 +0500

    zxc

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from "axios";
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 
 function App() {
   const [notes, setNotes] = useState(null);
+  const [isUpdate, setIsUpdate] = useState(false);
+
+  const inputInfo = useRef(null);
+  const inputTitle = useRef(null);
 
   useEffect(() => {
     axios.get(
@@ -20,10 +24,28 @@ function App() {
 
   }, []);
 
+  const addNote = () => {
+    axios.post('http://localhost:9090/api/notes/add', {
+      title: inputTitle.current.value,
+      info: inputInfo.current.value
+    }, {
+      withCredentials: false
+    }).then(() => { 
+      setIsUpdate(!isUpdate)
+    })
+  }
+
   return (
     <div className="App">
+      <label>ZZAGOGLOVOK</label>
+      <input ref={inputInfo} type="text"/>
+      <label>Opisanie</label>
+      <input ref={inputTitle} type="text"/>
+      <button onClick={() => addNote()}>
+        Add
+      </button>      
       {!!notes && notes.map((note, index) => (
-        <div key={index}>{note.title}</div>
+        <div key={index}>{note.title} - {note.info}</div>
       ))}
     </div>
   );
